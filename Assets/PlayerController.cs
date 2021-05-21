69513c4f33beea6dd9ee10f1436c5b95ea30c16e
Egor Kovel <egor.kovel1@gmail.com>
2021-05-21 17:03:13 +0300

    Review

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,33 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speedX = 10f;
-
+    [SerializeField] private float speedX = 10f; // TODO: use [SerializeField] with public Properties
     public float speedY = 5f;
-
     public float minY;
-    
     public float maxY;
-
     public Camera camera;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.Translate(Time.deltaTime * speedX, Input.GetAxis("Vertical") * speedY * Time.deltaTime, 0);
-        camera.transform.position = new Vector3(transform.position.x - 15, 1, transform.position.z - 5);
+        var playerDelta = new Vector3(Time.deltaTime * speedX, Input.GetAxis("Vertical") * speedY * Time.deltaTime, 0);
+        var roadDelta = Vector3.right * (Time.deltaTime * speedX);
         
-        // camera.transform.Translate(Vector3.right * (Time.deltaTime * speedX)); - почему не работает?
-        // transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
+        transform.Translate(playerDelta);
+        // camera.transform.position = new Vector3(transform.position.x - 15, 1, transform.position.z - 5);
+
+        camera.transform.Translate(roadDelta); // - почему не работает?
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
+
+        Debug.Log($"playerDelta = {playerDelta.x}, roadDelta = {roadDelta.x}");
     }
 }
