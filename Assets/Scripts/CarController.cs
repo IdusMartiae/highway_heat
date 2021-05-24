b0aeba7b1c438f18ea17f882f094a3b90fef1750
Anton Zhylko <anton.zhylko@itechart-group.com>
2021-05-24 18:43:06 +0300

    Add car model
    - Add car model
    - Add simple script to implement car movement

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gameObject;
+    [SerializeField] private float carOffset;
+    
+    void Update()
+    {
+        var newPosition  = new Vector3(transform.position.x, transform.position.y, gameObject.transform.position.z + carOffset);
+        transform.position = newPosition;
+    }
+}
