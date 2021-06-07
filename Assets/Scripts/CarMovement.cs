c2c0fa00857bfc36416ae22c235c6f5dbc9ec06a
Anton Zhylko <antonzhylko@gmail.com>
2021-06-07 14:57:34 +0300

    PoC Collider-dependent physics

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,18 +1,86 @@
+using System.Collections.Generic;
+using Entities;
+using Spawners;
 using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    [SerializeField] private GameObject roadSpawnerObject;
+    [SerializeField] private RoadSegmentSpawner roadSegmentSpawner;
     [SerializeField] private float xOffset;
+    [SerializeField] private float yOffset;
     [SerializeField] private float zOffset;
+
+    private float _carX;
+    private float _carY;
+    private float _carZ;
+    private float _carRotationX;
     
      private void Update()
-    {
-        var newPosition = new Vector3(
-            roadSpawnerObject.transform.position.x + xOffset,
-            transform.position.y,
-            roadSpawnerObject.transform.position.z + zOffset
-        );
-        transform.position = newPosition;
+     {
+         if (roadSegmentSpawner.ColliderFrontQueue.Count != 0)
+         {
+             CalculateCarTransform(); 
+             ChangeCarTransform();
+         }
+         
     }
+
+     private void ChangeCarTransform()
+     {
+         var newX = roadSegmentSpawner.transform.position.x + xOffset;
+         transform.position = new Vector3(newX, _carY, _carZ);
+         transform.eulerAngles = new Vector3(_carRotationX, 90f, 0f);
+     }
+
+     private void CalculateCarTransform()
+     {
+         var colliders = roadSegmentSpawner.ColliderFrontQueue;
+         
+         CalculateAverageCoordinates(out _carX, out _carY, out _carZ, colliders);
+         CalculateCarRotation(_carX, _carY, colliders);
+         
+         _carX += xOffset;
+         _carY += yOffset;
+         _carZ += zOffset;
+     }
+
+     private void CalculateAverageCoordinates(out float x, out float y, out float z, List<GameEntity> list)
+     {
+         x = 0f;
+         y = 0f;
+         z = 0f;
+         
+         foreach (var item in list)
+         {
+             var itemPosition = item.transform.position;
+             
+             x += itemPosition.x;
+             y += itemPosition.y;
+             z += itemPosition.z;
+             
+         }
+
+         x /= list.Count;
+         y /= list.Count;
+         z /= list.Count;
+         
+     }
+
+     private void CalculateCarRotation(float x, float y, List<GameEntity> list)
+     {
+         _carRotationX = 0f;
+
+         foreach (var item in list)
+         {
+             var itemPosition = item.transform.position;
+             
+             _carRotationX -= (itemPosition.y - y) / (itemPosition.x - x);
+    
+             
+         }
+
+         _carRotationX /= list.Count;
+
+         _carRotationX = Mathf.Atan(_carRotationX) * 180 / Mathf.PI;
+     }
 }
--- Assets/Scripts/Spawners/RoadSegmentSpawner.cs
+++ Assets/Scripts/Spawners/RoadSegmentSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Entities;
 using Factories;
 using UnityEngine;
@@ -9,18 +10,57 @@ namespace Spawners
         [SerializeField] private GameEntity colliderPrefab;
         [SerializeField] private int poolSize = 100;
         [SerializeField] private float destroyDistance = 10;
-        
+        [SerializeField] private int colliderFrontQueueSize;
+            
         private RoadSegmentFactory _roadSegmentFactory;
         
+        public List<GameEntity> ColliderFrontQueue { get; private set; }
+
         private void Awake()
         {
-            _roadSegmentFactory = new RoadSegmentFactory(poolSize, colliderPrefab, transform, destroyDistance);
+            InitializeSpawner();
         }
 
         private void Update()
         {
-            var roadSegment = _roadSegmentFactory.Create().gameObject;
+            CreateColliderAndAddToQueue();
+        }
+
+        private void OnDestroy()
+        {
+            ColliderFrontQueue.Clear();
+        }
+
+        private void InitializeSpawner()
+        {
+            _roadSegmentFactory = new RoadSegmentFactory(poolSize, colliderPrefab, transform, destroyDistance);
+            ColliderFrontQueue = new List<GameEntity>();
+        }
+
+        private void AddColliderToQueue(GameEntity colliderGameEntity)
+        {
+            if (ColliderFrontQueue.Count == colliderFrontQueueSize)
+            {
+                RemoveColliderFromQueue();
+            }
+            
+            ColliderFrontQueue.Add(colliderGameEntity);
+        }
+
+        private void RemoveColliderFromQueue()
+        {
+            if (ColliderFrontQueue.Count != 0)
+            {
+                ColliderFrontQueue.RemoveAt(0);
+            }
+        }
+
+        private void CreateColliderAndAddToQueue()
+        {
+            var roadSegment = _roadSegmentFactory.Create();
             roadSegment.transform.position = transform.position;
+            
+            AddColliderToQueue(roadSegment);
         }
     }
 }
