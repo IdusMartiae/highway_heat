c3749590f24f985a8bd895e0fedda9ab7ef4b9ae
Anton Zhylko <antonzhylko@gmail.com>
2021-06-24 17:17:57 +0300

    Replace parallax code
     - Replaced Translate with Lerp

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -13,11 +13,15 @@ public class ParallaxScript : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector3.left * (layerSpeed * Time.deltaTime));
+        var pos = _rectTransform.anchoredPosition;
+        var destPoint = new Vector2(pos.x - 2000, pos.y);
+        
+        _rectTransform.anchoredPosition = Vector2.Lerp(pos, destPoint, layerSpeed / 1000);
         
         if (_rectTransform.anchoredPosition.x <= -_rectTransform.rect.width)
         {
-            _rectTransform.anchoredPosition = Vector2.zero;
+            var yPosition = _rectTransform.anchoredPosition.y;
+            _rectTransform.anchoredPosition = new Vector2(0, yPosition);
         }
     }
     
